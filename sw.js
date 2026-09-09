6e64b3ddb21acefc04953dc9e2b6d8512e690ee3
natanlelonkiewicz <lelonkiewicznatan@gmail.com>
2026-09-09 22:35:14 +0100

    app development

--- sw.js
+++ sw.js
@@ -1,6 +1,136 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11571315
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.addEventListener(
+    "install",
+    function(){
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    function(event){
+
+        event.waitUntil(
+            self.clients.claim()
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "push",
+    function(event){
+
+        if(!event.data){
+
+            return;
+
+        }
+
+        let data;
+
+        try{
+
+            data =
+                event.data.json();
+
+        }
+        catch(error){
+
+            data = {
+
+                title:
+                    "Avspot",
+
+                body:
+                    event.data.text()
+
+            };
+
+        }
+
+        const title =
+            data.title ||
+            "Avspot";
+
+        const options = {
+
+            body:
+                data.body ||
+                "",
+
+            icon:
+                data.icon ||
+                "/icon-192.png",
+
+            badge:
+                data.badge ||
+                "/icon-192.png",
+
+            data:
+                data.data ||
+                {}
+
+        };
+
+        event.waitUntil(
+
+            self.registration
+                .showNotification(
+                    title,
+                    options
+                )
+
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "notificationclick",
+    function(event){
+
+        event.notification.close();
+
+        event.waitUntil(
+
+            clients.matchAll({
+                type: "window",
+                includeUncontrolled: true
+            }).then(function(clientList){
+
+                for(
+                    const client
+                    of clientList
+                ){
+
+                    if(
+                        "focus" in client
+                    ){
+
+                        return client.focus();
+
+                    }
+
+                }
+
+                if(
+                    clients.openWindow
+                ){
+
+                    return clients.openWindow(
+                        "/"
+                    );
+
+                }
+
+            })
+
+        );
+
+    }
+);
